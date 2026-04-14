18e16b3bbd166a57125bd7a4ab2aed2a8679a7ec
Matthew Fogle <mfogle@hcc-nd.edu>
2026-04-14 09:15:10 -0400

    adding a collapsible button

---
+++ collapsiblesections.js
@@ -0,0 +1,20 @@
+var coll = document.getElementsByClassName("collapsible");
+console.log(coll)
+console.log(coll.length)
+for(var i = 0; i < coll.length; i++)
+{
+    console.log("THe code is getting to the for loop")
+    coll[i].addEventListener("click",function(){
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if(content.style.display === "block")
+        {
+            content.style.display = "none";
+            console.log("This works if display === block")
+
+        }
+        else{
+            content.style.display = "block";
+        }
+    });
+}
